c2f1f75531811fce8cccd2739266e6488d9a777e
Ali Kheyrollahi <aliostad@gmail.com>
2013-10-23 20:54:53 +0100

    Initial Commit

---
+++ src/CqrsWebApiSample.Api/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CqrsWebApiSample.Api")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CqrsWebApiSample.Api")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("993d384c-9517-48b3-b97d-7d614c03421c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/CqrsWebApiSample.Api/Scripts/sample.js
@@ -0,0 +1,46 @@
+﻿// Wraps jquery's Ajax with 5LMT implementation
+
+
+(function (parent, $) {
+
+    
+    var oldAjax = $.ajax,
+        contentTypeName = "Content-Type";
+    
+    $.ajax = function(topSettings) {
+        topSettings = topSettings || {};
+        var originalBeforeSend = topSettings.beforeSend;
+       
+        topSettings.beforeSend = function (jqXHR, settings) {
+            if (originalBeforeSend)
+                originalBeforeSend(jqXHR, settings);
+            addFiveLevelsOfMediaType(jqXHR, settings);
+        };
+
+        oldAjax(topSettings);
+
+    };
+
+    function addFiveLevelsOfMediaType(jqXHR, settings) {
+        if (settings.data) {            
+            jqXHR.setRequestHeader(contentTypeName,
+                "application/json;domain-model=" + settings.data.constructor.name);
+        }
+    }
+    
+})(window, $);
+
+
+
+function ViewModel(name, surname) {
+    this.name = name;
+    this.surname = surname;
+}
+
+$("#a").click(function (ss) {
+    $.ajax({
+        method: "POST",
+        dataType: "json",
+        data: JSON.stringify(new ViewModel("ali", "Ostad"))
+    });
+});
